ba0e46abb304e5d54afada5e09caad66f90e1077
privateheart <781458261@qq.com>
2019-02-25 08:19:33 +0800

    StreamApi练习

--- java8/src/com/learn/demo/ch5/trade/TradeDemo.java
+++ java8/src/com/learn/demo/ch5/trade/TradeDemo.java
@@ -3,6 +3,7 @@ package com.learn.demo.ch5.trade;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.Comparator.comparing;
 
@@ -30,20 +31,23 @@ public class TradeDemo {
     public static void main(String[] args) {
         //(1) 找出2011年发生的所有的交易，并按照交易额排序（从低到高）
         System.out.println("(1) 找出2011年发生的所有的交易，并按照交易额排序（从低到高）");
-        transactions.stream().filter( transaction -> {return transaction.getYear() == 2011;}).sorted(comparing(Transaction::getValue)).forEach(t ->{
-            System.out.println(t);
-        });
-        //(2) 交易员都在哪些不同的城市工作过
+        transactions.stream().filter( transaction -> {return transaction.getYear() == 2011;}).sorted(comparing(Transaction::getValue)).forEach(System.out::println);
+        //(2) 交易员都在哪些不同的城市工作过  toSet 自带去重功能，根据hashCode
         System.out.println("(2) 交易员都在哪些不同的城市工作过");
-        transactions.stream().map( t -> {return t.getTrader().getCity();}).distinct().forEach(city -> System.out.println(city));
+        transactions.stream().map( t -> {return t.getTrader().getCity();}).distinct().forEach(System.out::println);
+
+//        transactions.stream().map(t -> {return t.getTrader().getCity();}).collect(Collectors.toSet()).forEach(city ->{System.out.println(city);});
         //(3) 查找所有来自于剑桥的交易员，并按照姓名排序
         System.out.println("(3) 查找所有来自于剑桥的交易员，并按照姓名排序");
-        transactions.stream().filter(t -> {return "Cambridge".equals(t.getTrader().getCity());}).map( t -> {return t.getTrader();}).sorted(comparing(Trader::getName)).forEach(t -> System.out.println(t));
+        transactions.stream().filter(t -> {return "Cambridge".equals(t.getTrader().getCity());}).map( t -> {return t.getTrader();}).sorted(comparing(Trader::getName)).forEach(System.out::println);
         //(4) 返回所有交易员的姓名字符串，按字母顺序排序
         System.out.println("(4) 返回所有交易员的姓名字符串，按字母顺序排序");
-        transactions.stream().map(t -> {return t.getTrader().getName();}).distinct().sorted(String::compareTo).forEach(n -> {
-            System.out.println(n);
+        String nameStr = transactions.stream().map(t -> {
+            return t.getTrader().getName();
+        }).reduce("", (n1, n2) -> {
+            return n1 + n2;
         });
+        System.out.println(nameStr);
         //(5) 有没有交易员是在米兰工作的
         System.out.println("(5) 有没有交易员是在米兰工作的");
         boolean isExists = transactions.stream().anyMatch(t -> {
@@ -52,7 +56,7 @@ public class TradeDemo {
         System.out.println(isExists);
         //(6) 打印生活在剑桥的交易员的所有交易额
         System.out.println("(6) 打印生活在剑桥的交易员的所有交易额");
-        transactions.stream().filter(t-> {return "Cambridge".equals(t.getTrader().getCity());});
+        transactions.stream().filter(t-> {return "Cambridge".equals(t.getTrader().getCity());}).forEach(System.out::println);
         //(7) 所有交易中，最高的交易额是多少？
         System.out.println("(7) 所有交易中，最高的交易额是多少？");
         Transaction maxTrade = transactions.stream().reduce(transactions.get(0), (t1, t2) -> {
